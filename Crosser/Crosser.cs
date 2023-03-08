0f8b568db1e44ce16d15be6c345b3c6284acb645
Aimen Ansari <umeaansa555@ed.amdsb.ca>
2023-03-08 09:21:10 -0500

    makin progress somewhat

--- Crosser/Crosser.cs
+++ Crosser/Crosser.cs
@@ -8,5 +8,30 @@ namespace Crosser
 {
     internal class Crosser
     {
+        public int x, y;
+        public int speed = 6;
+        public int width = 30, height = 10;
+
+        public void Move(string direction)
+        {
+            if (direction == "left")
+            {
+                x -= speed;
+            }
+            if (direction == "right")
+            {
+                x += speed;
+            }
+            if (direction == "up")
+            {
+                y -= speed;
+            }
+            if (direction == "down")
+            {
+                y += speed;
+            }
+
+
+        }
     }
 }
--- Crosser/Enemy.cs
+++ Crosser/Enemy.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Crosser
 {
     internal class Enemy
     {
+        public int x = 0 , y, width = 30 , height = 15, speed = 6;
+        
+
+        public Enemy(int _x, int _speed)
+        {
+            this.x = _x;
+            this.speed = _speed;
+        }
+
+
+        public void Move(int screenWidth, int screenHeight)
+        {
+            x += speed;
+
+            /*if (x > screenWidth - width || x < 0)
+            {
+                speed *= -1;
+            }*/
+        }
+        public bool Collision(Crosser c)
+        {
+            Rectangle enemyRec = new Rectangle(x, y, width, height);
+            Rectangle crosserRec = new Rectangle(c.x, c.y, c.width, c.height);
+
+            if (enemyRec.IntersectsWith(crosserRec))
+            {
+                #region
+                /*if (speed > 0)
+                {
+                    x = c.x - width;
+                }
+                else
+                {
+                    x = c.x + c.height;
+                }
+
+                speed *= -1; */
+                #endregion
+                return true;
+            }
+            return false;
+        }
     }
 }
