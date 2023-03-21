d1c123ef11e0160e02d7153b0b867cdfd6573b7a
Aimen Ansari <umeaansa555@ed.amdsb.ca>
2023-03-21 10:03:56 -0400

    working on endscreen

--- Crosser/EndScreen.Designer.cs
+++ Crosser/EndScreen.Designer.cs
@@ -28,19 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.menuButton = new System.Windows.Forms.Button();
+            this.endLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(393, 242);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(195, 83);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // menuButton
+            // 
+            this.menuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuButton.Location = new System.Drawing.Point(127, 242);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(195, 83);
+            this.menuButton.TabIndex = 4;
+            this.menuButton.Text = "Take another drive?";
+            this.menuButton.UseVisualStyleBackColor = true;
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endLabel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.endLabel.Location = new System.Drawing.Point(136, 65);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(442, 98);
+            this.endLabel.TabIndex = 3;
+            this.endLabel.Text = "COLLISION!!!";
             // 
             // EndScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.menuButton);
+            this.Controls.Add(this.endLabel);
             this.Name = "EndScreen";
             this.Size = new System.Drawing.Size(715, 405);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.Label endLabel;
     }
 }
